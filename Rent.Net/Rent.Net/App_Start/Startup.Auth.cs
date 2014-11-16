33f1d6a8909a5c09db7a2ac7a6fba92c2ad18f70
Gekctek <Gekctek@Gmail.com>
2014-11-15 18:13:17 -0800

    Removing all unneeded files. Starting expanding Entitiy Framework

--- Rent.Net/Rent.Net/App_Start/Startup.Auth.cs
+++ Rent.Net/Rent.Net/App_Start/Startup.Auth.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Owin;
 using Rent.Net.Models;
+using Rent.Net.Entities;
 
 namespace Rent.Net
 {
@@ -15,7 +15,7 @@ namespace Rent.Net
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(RentDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
---
+++ Rent.Net/Rent.Net/Entities/Request.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Rent.Net.Entities
+{
+    public class Request
+    {
+        public Request()
+        {
+
+        }
+
+        public int RequestId { get; set; }
+
+        public string PayerId { get; set; }
+        public string PayeeId { get; set; }
+        public virtual ApplicationUser Payer { get; set; }
+        public virtual ApplicationUser Payee { get; set; }
+    }
+}
