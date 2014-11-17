1f48cd12070b64ce65d1aa504c459e576e696862
jbourgoin <jabourg@gmail.com>
2014-11-16 22:30:53 -0800

    Added SumModel
    SumModel and Sum Feature

---
+++ Rent.Net/Rent.Net/Models/SumModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rent.Net.Models
+{
+    public class SumModel
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+
+        public SumModel(string name, decimal amount)
+        {
+            this.Name = name;
+            this.Amount = amount;
+        }
+
+    }
+}
